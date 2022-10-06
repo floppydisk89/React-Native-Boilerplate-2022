93f7543ea6acf5ba802b2d80106f31753a4eb5ff
Goncalo Marques <goncalo.an.marques@mediamonks.com>
2022-10-06 17:23:30 +0200

    added react re-animated animation library and configured it.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  //* react-native-reanimated/plugin MUST always go in last
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
