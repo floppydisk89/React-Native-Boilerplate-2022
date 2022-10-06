09ec42df66b76f7a1c728fc41ec44d08f23ce772
Goncalo Marques <goncalo.an.marques@mediamonks.com>
2022-10-06 17:16:40 +0200

    added api service with axios

---
+++ data/host.ts
@@ -0,0 +1,6 @@
+export default {
+  baseUrl: {
+    development: 'localhost:3000',
+    production: 'mysite.com',
+  },
+};
---
+++ services/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import host from '../data/host';
+import config from '../config';
+
+const api = axios.create({
+  baseURL: `${
+    config.mode === 'production'
+      ? host.baseUrl.production
+      : host.baseUrl.development
+  }/api/`,
+  timeout: 1000,
+  headers: { 'Cross-Origin-Resource-Policy': 'same-origin' },
+});
+
+export default api;
