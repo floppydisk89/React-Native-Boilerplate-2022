202af45aa49bbc4389ce7e23a1a6ee2762e55c6e
Goncalo Marques <goncalo.an.marques@mediamonks.com>
2022-10-06 15:13:17 +0200

    removed testing lib due to bugs and errors. Added better eslint config.

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-param-reassign': [2, { props: false }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'linebreak-style': ['ignore', 'windows'],
+  },
+};
---
+++ __tests__/root.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react-native';
+import Root from '../views/Root';
+
+it('renders', () => {
+  render(<Root />);
+});
