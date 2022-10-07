88889be10dc7b7f9253791a851f4f87dc2dd8dae
Goncalo Marques <goncalo.an.marques@mediamonks.com>
2022-10-07 17:55:06 +0200

    removed incorrect way of working with statusbar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
-import statusbar from './config/statusbar';
 import Root from './views/Main';
 import store from './store';
 
 export default function App() {
-  const statusBarMode = statusbar.mode;
   return (
     <Provider store={store}>
-      <StatusBar style={statusBarMode} />
+      <StatusBar style="auto" />
       <Root />
     </Provider>
   );
